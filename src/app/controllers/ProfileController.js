0f35b8f16e81d3d2795003b31a10e13b51e2e2c0
Happyboy171 <luckyboy1992330@mail.com>
2017-02-15 23:13:58 +0800

    Add some profile infos in profileController.

--- src/app/controllers/ProfileController.js
+++ src/app/controllers/ProfileController.js
@@ -22,6 +22,58 @@
       'Our focus is JavaScript development and User Interface design.',
       postalCode : '220007'
     };
+    vm.user = {
+      title: 'Admin',
+      email: 'contact@flatlogic.com',
+      firstName: '',
+      lastName: '' ,
+      company: 'FlatLogic Inc.' ,
+      address: 'Fabritsiusa str, 4' ,
+      city: 'Minsk' ,
+      state: '' ,
+      biography: 'We are young and ambitious full service design and technology company. ' +
+      'Our focus is JavaScript development and User Interface design.',
+      postalCode : '220007'
+    };
+    vm.user = {
+      title: 'Admin',
+      email: 'contact@flatlogic.com',
+      firstName: '',
+      lastName: '' ,
+      company: 'FlatLogic Inc.' ,
+      address: 'Fabritsiusa str, 4' ,
+      city: 'Minsk' ,
+      state: '' ,
+      biography: 'We are young and ambitious full service design and technology company. ' +
+      'Our focus is JavaScript development and User Interface design.',
+      postalCode : '220007'
+    };
+    vm.user = {
+      title: 'Admin',
+      email: 'contact@flatlogic.com',
+      firstName: '',
+      lastName: '' ,
+      company: 'FlatLogic Inc.' ,
+      address: 'Fabritsiusa str, 4' ,
+      city: 'Minsk' ,
+      state: '' ,
+      biography: 'We are young and ambitious full service design and technology company. ' +
+      'Our focus is JavaScript development and User Interface design.',
+      postalCode : '220007'
+    };
+    vm.user = {
+      title: 'Admin',
+      email: 'contact@flatlogic.com',
+      firstName: '',
+      lastName: '' ,
+      company: 'FlatLogic Inc.' ,
+      address: 'Fabritsiusa str, 4' ,
+      city: 'Minsk' ,
+      state: '' ,
+      biography: 'We are young and ambitious full service design and technology company. ' +
+      'Our focus is JavaScript development and User Interface design.',
+      postalCode : '220007'
+    };
   }
 
 })();
